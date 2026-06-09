cfb63c0e7322dd3e2f62ab3c91b03ae8c5923a7d
SportsVision <sportsvisionmanager@gmail.com>
2026-06-08 21:54:02 -0500

    auto: settings.py

--- sportsvision/settings.py
+++ sportsvision/settings.py
@@ -127,6 +127,25 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'},
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
 # Auth redirects
 LOGIN_URL = '/usuarios/login/'
 LOGIN_REDIRECT_URL = '/dashboard/'
