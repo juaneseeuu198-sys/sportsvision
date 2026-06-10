8d78113e875d56b3fa71d48fcb41b1df4e1d431f
SportsVision <sportsvisionmanager@gmail.com>
2026-06-10 16:17:14 -0500

    auto: urls.py

--- apps/progress/urls.py
+++ apps/progress/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.calendario_progreso, name='calendario_progreso'),
     path('anotar/', views.anotar_dia, name='anotar_dia'),
+    path('gcal/sync/', views.gcal_sync_all, name='gcal_sync_all'),
 ]
